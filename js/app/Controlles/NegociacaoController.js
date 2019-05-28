4e7539f0d28a73a6a88ded1bd6c6e81021ab1335
farleybsd <farley.t.i@hotmail.com>
2019-05-28 15:25:07 -0300

    Adicionado Controller Model Negociacoes

---
+++ js/app/Controlles/NegociacaoController.js
@@ -0,0 +1,9 @@
+class NegociacaoController{
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("valor");
+    }
+}
